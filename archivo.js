ab1650f547abe061da2c4f114b4a0e84696523d3
katyprieto <katyanaprieto@hotmail.com>
2022-03-09 18:06:46 -0300

    commit java

--- archivo.js
+++ archivo.js
@@ -2,7 +2,7 @@ function puedeSubir(altura, acompanada){
     if (altura >= 1.40 && altura <= 2) 
         return true;
     }
-    if( altura <= 1.40 && altura >= 1.20 && acompanada){
+    if( altura <= 1.4555 && altura >= 1 && acompanada){
         return true;
     }
     else{
