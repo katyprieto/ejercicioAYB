a12efc072bea206ed7dd6f4c9dd7adcf11f6f470
cristian <cristiandosespigas@gmail.com>
2022-03-09 18:05:17 -0300

    subviendo archivo javascript

---
+++ archivo.js
@@ -0,0 +1,10 @@
+function puedeSubir(altura, acompanada){
+    if (altura >= 1.40 && altura <= 2) 
+        return true;
+    }
+    if( altura <= 1.40 && altura >= 1.20 && acompanada){
+        return true;
+    }
+    else{
+        return false;
+    }
